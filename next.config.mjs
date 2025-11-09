e0af443fcb6df63e1a85e251fd49594ea2caaa8d
youssifkhalid <145773104+youssifkhalid@users.noreply.github.com>
2025-11-09 12:00:17 +0000

    Configure static export and uploaded all files

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // **تأكد من وجود هذا السطر**
-    output: 'export', 
-    
-      // ... باقي إعداداتك هنا ...
-      };
-      
-      export default nextConfig; // أو module.exports = nextConfig;
-       */
+  output: 'export', 
+  };
+  
+  export default nextConfig;
+   
